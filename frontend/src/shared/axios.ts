95ce3eee228ff57d1e6a3b29ddfaf214699c39b6
Princh Canal <princhcanal@gmail.com>
2021-10-28 11:48:50 +0800

    setup heroku deployment

--- frontend/src/shared/axios.ts
+++ frontend/src/shared/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.BASE_PATH || 'http://localhost:3000/api/v1';
+const baseURL =
+  process.env.BASE_URL + '/api/v1' || 'http://localhost:3000/api/v1';
 
 export const axiosInstance = axios.create({
   baseURL,
