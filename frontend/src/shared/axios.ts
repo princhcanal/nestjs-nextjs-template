e48ee295c542dbd4b26d10e015232fd4a58c27bb
Princh Canal <princhcanal@gmail.com>
2021-10-28 18:08:49 +0800

    feat(pr preview): setup for pr preview

--- frontend/src/shared/axios.ts
+++ frontend/src/shared/axios.ts
@@ -7,7 +7,7 @@ if (process.env.VERCEL_GIT_IS_PULL_REQUEST === '1') {
   reviewAppUrl = `https://nest-next-template-pr-${prNumber}.herokuapp.com`;
 }
 
-reviewAppUrl = `https://nest-next-template-pr-${26}.herokuapp.com`;
+reviewAppUrl = `https://nest-next-template-pr-${27}.herokuapp.com`;
 
 const baseURL =
   process.env.NEXT_PUBLIC_BASE_URL ||
