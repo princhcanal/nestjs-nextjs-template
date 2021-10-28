9e5e7eaabb4e94f90ebe35b9b4dbc64ec81821f5
Princh Canal <princhcanal@gmail.com>
2021-10-28 13:05:54 +0800

    chore(deployment): change env var name

--- frontend/src/shared/axios.ts
+++ frontend/src/shared/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.BASE_PATH || 'http://localhost:3000/api/v1';
+const baseURL = process.env.BASE_URL || 'http://localhost:3000/api/v1';
 
 export const axiosInstance = axios.create({
   baseURL,
