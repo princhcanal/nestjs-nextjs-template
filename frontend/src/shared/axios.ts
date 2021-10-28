c67e7546e6ca16792b9ef700880ee19b9b82e03e
Princh Canal <princhcanal@gmail.com>
2021-10-28 17:56:38 +0800

    add console.log

--- frontend/src/shared/axios.ts
+++ frontend/src/shared/axios.ts
@@ -12,6 +12,17 @@ const baseURL =
   reviewAppUrl ||
   'http://localhost:3000/api/v1';
 
+// tslint:disable:no-console
+console.log('axios.ts');
+// tslint:disable:no-console
+console.log('is pr:', process.env.VERCEL_GIT_IS_PULL_REQUEST);
+// tslint:disable:no-console
+console.log('pr number:', process.env.VERCEL_GIT_PULL_REQUEST_NUMBER);
+// tslint:disable:no-console
+console.log('reviewAppUrl:', reviewAppUrl);
+// tslint:disable:no-console
+console.log('next public url:', process.env.NEXT_PUBLIC_BASE_URL);
+
 export const axiosInstance = axios.create({
   baseURL,
   withCredentials: true,
--- frontend/src/shared/providers/ApiProvider.tsx
+++ frontend/src/shared/providers/ApiProvider.tsx
@@ -12,6 +12,17 @@ if (process.env.VERCEL_GIT_IS_PULL_REQUEST === '1') {
 const baseURL =
   process.env.NEXT_PUBLIC_BASE_URL || reviewAppUrl || 'http://localhost:3000';
 
+// tslint:disable:no-console
+console.log('ApiProvider.tsx');
+// tslint:disable:no-console
+console.log('is pr:', process.env.VERCEL_GIT_IS_PULL_REQUEST);
+// tslint:disable:no-console
+console.log('pr number:', process.env.VERCEL_GIT_PULL_REQUEST_NUMBER);
+// tslint:disable:no-console
+console.log('reviewAppUrl:', reviewAppUrl);
+// tslint:disable:no-console
+console.log('next public url:', process.env.NEXT_PUBLIC_BASE_URL);
+
 interface ApiProviderProps {
   children: ReactNode;
 }
