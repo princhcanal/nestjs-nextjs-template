71c33c7b4810e31f50672b8045c0be07755e09e4
Princh Canal <princhcanal@gmail.com>
2022-08-18 15:50:23 +0800

    chore(vercel): create frontend build script and ignore builds script

---
+++ frontend/scripts/build-frontend.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source node_modules/.bin/vercel-is-pull-request --auth $1
+source ./scripts/export-pr-number.sh
+npm run build 
+./scripts/wait-on-api.sh
--- frontend/scripts/wait-on-api.sh
+++ frontend/scripts/wait-on-api.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [[ ! -z "$NEXT_PUBLIC_HEROKU_PREVIEW_URL_PREFIX" && ! -z "$VERCEL_GIT_PULL_REQUEST_NUMBER" ]]; then 
   npm run wait-on -- $(echo $NEXT_PUBLIC_HEROKU_PREVIEW_URL_PREFIX)$(echo $VERCEL_GIT_PULL_REQUEST_NUMBER).herokuapp.com/api/v1
 elif [[ ! -z "$NEXT_PUBLIC_BASE_URL" ]]; then
