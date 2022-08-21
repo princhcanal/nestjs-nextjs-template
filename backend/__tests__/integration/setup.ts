5acb136167bdffd67e6cdaed1bb81954606e680c
Princh Canal <princhcanal@gmail.com>
2022-08-21 10:22:31 +0800

    test(backend): set expiration dates on test tokens to 9999 years

--- backend/__tests__/integration/setup.ts
+++ backend/__tests__/integration/setup.ts
@@ -8,8 +8,8 @@ const hook =
   (method: Methods, isAdmin?: boolean) =>
   (url: string, callback?: CallbackHandler): Test => {
     const accessToken = isAdmin
-      ? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyY2YzODY3MC0wYThhLTQxZTktOTAxOC1lOGI4YTliMzY0ODYiLCJpYXQiOjE2NjA5ODI4MzQsImV4cCI6MTY2MTA2OTIzNH0.i7DMMpaHmIye-oDqjLFb6zaFNNrIY2NJ2nC3ucRVt-k'
-      : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyZjU0Y2EwYi1lMzg5LTRlMTctYTk3OC0wY2I5OGUwZjdhNDYiLCJpYXQiOjE2NjA5NjA5NzcsImV4cCI6MTY2MTA0NzM3N30.uHfDdAFQetZoViVNv5BsxsygVD4WgAI8ClOZBWBJ7tw';
+      ? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyY2YzODY3MC0wYThhLTQxZTktOTAxOC1lOGI4YTliMzY0ODYiLCJpYXQiOjE2NjEwNDg0MjIsImV4cCI6MzE3MjA1NDkwODIyfQ.7NCz6kII-5qYeTW_eEnW-gtKOnMRCdgePzb9sZ_hlLg'
+      : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyZjU0Y2EwYi1lMzg5LTRlMTctYTk3OC0wY2I5OGUwZjdhNDYiLCJpYXQiOjE2NjEwNDgzNTcsImV4cCI6MzE3MjA1NDkwNzU3fQ.POlRWQVPd24FDf14kLJ4u92nKRoBa-79-pxOoSZ-QAw';
 
     return request[method](url, callback).set(
       'Authorization',
