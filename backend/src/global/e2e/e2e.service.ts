b0856bd54f697ef2206ba326ba26af89d4fbceeb
Princh Canal <princhcanal@gmail.com>
2022-08-21 09:54:48 +0800

    chore(e2e): prevent reset of data restarting server

--- backend/src/global/e2e/e2e.service.ts
+++ backend/src/global/e2e/e2e.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
-import { execSync } from 'child_process';
 import { CustomLogger } from '../../shared/custom-logger';
+import { PrismaService } from '../prisma/prisma.service';
+import { TestDataService } from '../test-data/test-data.service';
 
 @Injectable()
 export class E2EService {
   private readonly logger = new CustomLogger(E2EService.name);
 
-  public resetTestData() {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly testDataService: TestDataService
+  ) {}
+
+  public async resetTestData() {
     this.logger.log('=============================================');
     this.logger.log('RESETTING BACKEND TEST DATA');
 
-    // resets db
-    // TODO: prevent command from restarting dev server
-    execSync('sleep 2');
-    execSync('prisma migrate reset --force');
+    await this.resetDb();
 
-    // TODO: bring back once prisma migrate reset --force stops restarting dev server
     // generates test data
-    // await this.testDataService.resetTestData();
+    await this.testDataService.resetTestData();
 
     this.logger.log('DONE RESETTING BACKEND TEST DATA');
     this.logger.log('=============================================');
     this.logger.log('');
+  }
+
+  private async resetDb() {
+    await this.prismaService.$executeRaw`
+      CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
+      DECLARE
+          statements CURSOR FOR
+              SELECT tablename FROM pg_tables
+              WHERE tableowner = username AND schemaname = 'public';
+      BEGIN
+          FOR stmt IN statements LOOP
+              EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
+          END LOOP;
+      END;
+      $$ LANGUAGE plpgsql;
+    `;
+
+    await this.prismaService.$executeRaw`SELECT truncate_tables('template')`;
   }
 }
