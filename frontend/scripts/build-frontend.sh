19c7439a7148cfa9278cd56da25e312550b07406
Princh Harold Cañal <princhcanal@gmail.com>
2022-12-25 21:40:18 +0800

    chore(build): remove wait on script in frontend build step

--- frontend/scripts/build-frontend.sh
+++ frontend/scripts/build-frontend.sh
@@ -2,5 +2,5 @@
 
 source node_modules/.bin/vercel-is-pull-request --auth $1
 source ./scripts/export-pr-number.sh
-npm run build 
-./scripts/wait-on-api.sh
+npm run build
+# ./scripts/wait-on-api.sh
