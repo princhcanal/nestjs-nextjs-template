a84bf7105c84d717cc83f34e8eec43828a248d93
Princh Harold Cañal <princhcanal@gmail.com>
2022-12-25 21:33:08 +0800

    feat(landing): add login and register links in landing page

--- frontend/src/modules/index/components/Landing.tsx
+++ frontend/src/modules/index/components/Landing.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import { Heading, HStack, VStack } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 export const Landing = () => {
-  return <Heading>NextJS/NestJS Template</Heading>;
+  return (
+    <VStack spacing={8}>
+      <Heading>NextJS/NestJS Template</Heading>
+
+      <HStack justify='center' spacing={8}>
+        <NextLink href='/login'>Log In</NextLink>
+        <NextLink href='/register'>Register</NextLink>
+      </HStack>
+    </VStack>
+  );
 };
